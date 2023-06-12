0fa9dd0ff8e247398f4803578a75bda10b468d02
teddy004 <tewodrosatirsaw4@gmail.com>
2023-06-12 14:22:55 +0000

    using angular router module

--- homes-app/src/main.ts
+++ homes-app/src/main.ts
@@ -1,5 +1,12 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { HomeComponent } from './app/home/home.component';
+import routeConfig from './app/routes';
 
-bootstrapApplication(AppComponent)
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routeConfig)
+  ]
+})
   .catch(err => console.error(err));
